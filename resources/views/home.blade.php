0c5064df0a5210fc6713061a4e60a395c0b4c99d
Bayode Aderinola <bayodesegun@gmail.com>
2017-01-19 16:54:27 +0100

    Make basic template for content delivery on /home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"><b>Dashboard</b></div>
 
                 <div class="panel-body">
                     <?php echo $message ?>
@@ -15,7 +15,6 @@
                 @if ($location == 'unknown')
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="GET" action="{{ url('/home') }}">
-                        {{ csrf_field() }}
                         <p>Please select your location to continue.</p>
                         <div class="form-group">
                             <label for="location" class="col-md-3 control-label">Location</label>
@@ -41,5 +40,53 @@
             </div>
         </div>
     </div>
+
+    <!-- Now for the three panels: 'Power plan', 'Related posts', 'Your posts/commments'  -->
+    <div class="row">
+        <!-- Power Plan details here -->
+        <div class="col-md-3 no-right-pad">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Power Plan</b></div>
+
+                <div class="panel-body">
+                    Location: {{ $location }}.
+                </div>                
+            </div>
+        </div>
+
+        <!-- Related Posts here -->
+        <div class="col-md-6 no-left-pad no-right-pad">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Related Posts</b></div>
+
+                <div class="panel-body">
+                    Posts: 
+                </div>                
+            </div>
+        </div>
+
+        <!-- User's posts and comments here -->
+        <div class="col-md-3 no-left-pad">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Your Posts & Comments</b></div>
+
+                <div class="panel-body">
+                    Posts: 
+                </div>                
+            </div>
+        </div>
+    </div>
 </div>
+<style>
+    .no-left-pad {
+        padding-left: 0;        
+    }
+
+    .no-right-pad {
+        padding-right: 0;        
+    }
+    .panel {
+        margin-bottom: 0;
+    }
+</style>
 @endsection
