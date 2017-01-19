4d8dafbbf95a067a59dcdaea7d39200bcfdbad5e
Bayode Aderinola <bayodesegun@gmail.com>
2017-01-19 14:36:15 +0100

    show location selector if location is not set

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-           
+
     }
 
     /**
@@ -22,8 +22,16 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        // Handle setting of location...
+        if ($loc = $request->input('location')) {
+            session(['location' => $loc ]);
+        }
+
+        // Fetch the 'location' session variable to customize user experience
+        $location = session('location', 'unknown');
+
         // Default message for users not logged in
         $message = 'Welcome Guest, you are not logged in. To post or comment, please <a href="/login">Login</a> or <a href="/register">Register</a>.';
 
@@ -31,7 +39,10 @@ class HomeController extends Controller
             // The user is logged in...
             $user = Auth::user()->name;
             $message = "Welcome $user, your are logged in.";
+
+            // TODO: 'remember' user location from profile
+            // $location = Auth::user()->location;
         }
-        return view('home', ['message' => $message]);
+        return view('home', ['message' => $message, 'location' => $location]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,34 @@
                 <div class="panel-body">
                     <?php echo $message ?>
                 </div>
+
+                <!-- Show location selector if location is not set -->
+                @if ($location == 'unknown')
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="GET" action="{{ url('/home') }}">
+                        {{ csrf_field() }}
+                        <p>Please select your location to continue.</p>
+                        <div class="form-group">
+                            <label for="location" class="col-md-3 control-label">Location</label>
+
+                            <div class="col-md-6">
+                                <select id="location" type="text" class="form-control" name="location" required autofocus>
+                                  <option value="oluyole-ext">Oluyole Extension</option>
+                                  <option value="gbagi-ibadan">Gbagi Market Ibadan</option>
+                                  <option value="oba-ile-est-akure">Oba-Ile Estate Akure</option>
+                                  <option value="texaco-akure">Texaco Akure</option>
+                                </select>
+                            </div>
+
+                            <div class="col-md-3">
+                                <button type="submit" class="btn btn-primary">
+                                    Set!
+                                </button>
+                            </div>
+                        </div> 
+                    </form>
+                </div>
+                @endif
             </div>
         </div>
     </div>
