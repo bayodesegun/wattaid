1b2d27c391d595b1ae8c3897eacc4346d1c3e5c5
Bayode Aderinola <bayodesegun@gmail.com>
2017-01-21 06:33:16 +0100

    Add PostController to handle actions on posts table

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validate the data
+        $this->validate($request, [
+            'user' => 'required|max:255',
+            'location' => 'required|max:255',
+            'type' => 'required|max:1',
+            'title' => 'required|max:255',
+            'message' => 'required|min:5',
+           
+        ]);
+
+        // Save the request data if validation passes        
+        Post::create([
+            'user' => $request->user,
+            'location' => $request->location,
+            'type' => $request->type,
+            'post_title' => $request->title,
+            'content' => $request->message,
+            'post_id' => uniqid($data['type'], true),
+        ]); 
+
+        return redirect('home')->with('status', 'Post saved successfully!');      
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
