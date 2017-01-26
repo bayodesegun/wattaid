d4118ec8e8afdd4931c9549aabfcd1929405acb3
Timilehin Aderinola <timiderinola@gmail.com>
2017-01-26 17:36:41 +0100

    Added header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,82 +3,73 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
         <title>WattAid</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->        
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}" media="screen" data-name="skins">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/layout/wide.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/switcher.css')}}" media="screen">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 100px;
-
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: -20px;
-            }
-
-            a:link {
-                text-decoration: none;
-            }
-
-            .red-text {
-                color: red;
-            }
-
-        </style>
+        <script src="{{asset('js/vendor/modernizr-2.6.2-respond-1.1.0.min.js')}}"></script>
+    
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
+        
+        <!--Start Header-->
+        <header id="header">
+            <!-- Container -->
+            <div class="container">
+                <div class="row">
+                <!-- Logo / Mobile Menu -->
+                    <div class="col-sm-2 col-md-2 col-lg-2">
+                        <div id="mobile-navigation">
+                            <a href="#menu" class="menu-trigger">
+                                <i class="fa fa-bars"></i>
+                            </a>
+                        </div>
+                        
+                        <div id="logo">
+                            <h1><a href="index.html"><img src="img/logo.png" alt="WATTAID" /></a></h1>
+                        </div>
+                    </div>
+
+
+                    <!-- Navigation
+                    ================================================== -->
+                    <div class="col-lg-10 col-md-10">
+                        <nav id="navigation" class="menu">
+                            <ul id="responsive">
+                                <li><a href="/" id="current">Home</a>
+                                </li>
+                                <li><a href="#">About</a>
+                                    <ul>
+                                        <li><a href="services.html">Scepter Concept</a></li>
+                                        <li><a href="faq.html">Wattaid</a></li>
+                                        <li><a href="404-page.html">404 Page</a></li>
+                                    </ul>
+                                </li>
+
+                                <li><a href="/home">Forum</a>
+                                </li>
+
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <!-- Container / End -->
+        </header>
+    <!--End Header-->
+        <div class="container">
+            <div>
                 
-                <div class="title m-b-md">
+                <div class="jumbotron">
                     <a href="/home">WattAid</a>                    
                 </div>
                 <p class = "red-text">Your one-stop power info site. Click to enter.</p>                
