f18c8a4676b64252ba5e5491a3a141654af515da
Bayode Aderinola <bayodesegun@gmail.com>
2017-02-25 15:31:50 +0100

    BUG FIX: Only show sign up button if user is not logged in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,11 +18,13 @@
                         <p>You're not alone. Whatever you need. Wherever you are.</p>
                     </div>
                 </div>
+                @if (Auth::guest())
                 <div class="col-sm-3 col-md-3 col-lg-3">
                     <a class="btn btn-large btn-default" href="/register">
                         <b>Sign Up Now</b>
                     </a>
                 </div>
+                @endif
             </div>
         </div>
     </section>
