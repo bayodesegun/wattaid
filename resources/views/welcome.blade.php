883e6fc5b7fb737492375dc95310367794ee595f
Bayode Aderinola <bayodesegun@gmail.com>
2017-02-25 16:12:43 +0100

    Remove dead and unnecessary links on the welcome (home) page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,8 +85,8 @@
                             <figure>
                                 <div class="bwWrapper touching medium">
                                     <img src="img/portfolio/work_1.png" alt="" />
-                                    <a href="img/portfolio/full/portfolio_1.jpg" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
-                                    <a href="portfolio_single.html" class="hover-link">
+                                    <a href="img/portfolio/work_1.png" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
+                                    <a class="hover-link">
                                         <i class="fa fa-link"></i>
                                     </a>
                                 </div>
@@ -104,8 +104,8 @@
                             <figure>
                                 <div class="bwWrapper touching medium">
                                     <img src="img/portfolio/work_2.png" alt="" />
-                                    <a href="img/portfolio/full/portfolio_2.jpg" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
-                                    <a href="portfolio_single.html" class="hover-link"><i class="fa fa-link"></i></a>
+                                    <a href="img/portfolio/work_2.png" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
+                                    <a class="hover-link"><i class="fa fa-link"></i></a>
                                 </div>
                                 <figcaption class="item-description">
                                     <h5>Inverters</h5>
@@ -121,8 +121,8 @@
                             <figure>
                                 <div class="bwWrapper touching medium">
                                     <img src="img/portfolio/work_3.png" alt="" />
-                                    <a href="img/portfolio/full/portfolio_3.jpg" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
-                                    <a href="portfolio_single.html" class="hover-link"><i class="fa fa-link"></i></a>
+                                    <a href="img/portfolio/work_3.png" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
+                                    <a class="hover-link"><i class="fa fa-link"></i></a>
                                 </div>
                                 <figcaption class="item-description">
                                     <h5>High Power Inverters</h5>
@@ -138,8 +138,8 @@
                             <figure>
                                 <div class="bwWrapper touching medium">
                                     <img src="img/portfolio/work_4.png" alt="" />
-                                    <a href="img/portfolio/full/portfolio_4.jpg" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
-                                    <a href="portfolio_single.html" class="hover-link"><i class="fa fa-link"></i></a>
+                                    <a href="img/portfolio/work_4.png" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
+                                    <a class="hover-link"><i class="fa fa-link"></i></a>
                                 </div>
                                 <figcaption class="item-description">
                                     <h5>Solar Power</h5>
@@ -155,8 +155,8 @@
                             <figure>
                                 <div class="bwWrapper touching medium">
                                     <img src="img/portfolio/work_5.png" alt="" />
-                                    <a href="img/portfolio/full/portfolio_5.jpg" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
-                                    <a href="portfolio_single.html" class="hover-link"><i class="fa fa-link"></i></a>
+                                    <a href="img/portfolio/work_5.png" class="hover-zoom mfp-image" ><i class="fa fa-search"></i></a>
+                                    <a class="hover-link"><i class="fa fa-link"></i></a>
                                 </div>
                                 <figcaption class="item-description">
                                     <h5>Power Banks</h5>
@@ -193,9 +193,9 @@
                                 <h3>Excellent Power Services</h3>
                                 <p>Scepter Concept Engineering provices products such as off- grid inverters (on-line and off-line inverter), Solar Panels with charge controllers of different ranges, Electrical wiring and installation of electrical panels.</p>
                                 <ul class="list_style circle">
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> Competent Engineers and Technicians.</a></li>
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> Top-notch testing equipment.</a></li>
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> Strict measures towards achieving <b>zero fault occurences.</b></a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> Competent Engineers and Technicians.</a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> Top-notch testing equipment.</a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> Strict measures towards achieving <b>zero fault occurences.</b></a></li>
                                 </ul>
                             </div><!-- end popular posts --> 
                             
@@ -203,9 +203,9 @@
                                 <h3>Up to date information</h3>
                                 <p>The site is daily updated with information from users and the power distribution companies. You can get to know the daily power plan for your area.</p>
                                 <ul class="list_style">
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> Daily power plan.</a></li>
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> Help on power problems.</a></li>
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> Interact with other power users.</a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> Daily power plan.</a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> Help on power problems.</a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> Interact with other power users.</a></li>
                                 </ul>
                             </div><!-- end popular articles --> 
                             
@@ -213,9 +213,9 @@
                                 <h3>Online interaction with other users</h3>
                                 <p><a href="/register">Create an account</a>, and you'll see have access to posts from all power users in your area. You can post questions or observations, as well as give answers to questions.</p>
                                 <ul class="list_style square">
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> Make and view posts based on location.</a></li>
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> Give or receive help in real time.</a></li>
-                                    <li><a href="#"><i class="fa fa-angle-right"></i> You are not alone</a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> Make and view posts based on location.</a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> Give or receive help in real time.</a></li>
+                                    <li><a ><i class="fa fa-angle-right"></i> You are not alone</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -254,7 +254,7 @@
                     <div class="our_clients">
                         <ul class="client_items clearfix">
                             <li>
-                                <a class="bwWrapper text" href="services.html"  data-placement="bottom" data-toggle="tooltip" title="Scepter Concept" ><img src="img/scepterconcept.png" alt="" /></a>
+                                <a class="bwWrapper text" data-placement="bottom" data-toggle="tooltip" title="Scepter Concept" ><img src="img/scepterconcept.png" alt="" /></a>
                             </li>
                         </ul><!--/ .client_items-->
                     </div>
